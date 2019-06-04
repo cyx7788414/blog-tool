f93a1455fcac06c90cbe69d4ec9745997b95b6e9
kabi <530853620@qq.com>
2019-06-05 01:11:43 +0800

    save 2019/06/05

--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const yargs = require("yargs");
 const argv = yargs
     .usage('help to write blog with markdown & generate data like index/classification')
-    .option('init', {
-    alias: 'i',
-    description: 'init base file'
-})
     .help('help').alias('help', 'h')
     .version('version', '1.0.0').alias('version', 'v')
+    .command('init', 'init base path', {
+    path: {
+        alias: 'p',
+        description: 'bath path',
+        default: './',
+        type: 'string'
+    }
+})
+    .command('add', 'add new article', {
+    name: {
+        alias: 'n',
+        description: 'name for the new article',
+        type: 'string',
+        demandOption: true
+    }
+})
     .argv;
 console.log(argv);
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,25 @@ import  * as yargs  from 'yargs';
 
 const argv = yargs
     .usage('help to write blog with markdown & generate data like index/classification')
-    .option('init', {
-        alias: 'i',
-        description: 'init base file'
-    })
     .help('help').alias('help', 'h')
     .version('version', '1.0.0').alias('version', 'v')
+    .command('init', 'init base path', {
+        path: {
+            alias: 'p',
+            description: 'bath path',
+            default: './',
+            type: 'string'
+        }
+    })
+    .command('add', 'add new article', {
+        name: {
+            alias: 'n',
+            description: 'name for the new article',
+            type: 'string',
+            demandOption: true
+        }
+    })
+    .command('delete', '')
     .argv;
 
 console.log(argv);
