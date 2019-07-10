4640e9a8ff0d891276a92b88077ce9bc44fd798d
chenyuxuan <chenyuxuan@fiberhome.com>
2019-07-10 17:23:18 +0800

    attr temp

--- src/command/attr.ts
+++ src/command/attr.ts
@@ -4,7 +4,15 @@ import common from '../common/common';
 const attr = async (argv: yargs.Arguments<any>): Promise<void> => {
     console.log(argv);
     let answer: Promise<string> = await common.inquirer.make([
-
+        {
+            type: 'list',
+            name: 'target',
+            message: 'please choice the target',
+            choices: [
+                'type',
+                'tag'
+            ]
+        }
     ]);
 };
 
